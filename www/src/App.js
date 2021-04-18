18865fb0b2f9d38e7ff68e7a30e635f5237878b9
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-04-18 10:56:45 +1000

    forgot login page

--- www/src/App.js
+++ www/src/App.js
@@ -166,12 +166,23 @@ function App() {
       </Alert>
         <Row>
           <Col md="12">
+            {idToken.length > 0 ? (
               <BrowserRouter>
                 <Switch>
-                  <Route path="/addhire"><AddHire updateAlert={updateAlert} addToDo={addToDo} /></Route>
+                  <Route path="/addhire"><AddHire updateAlert={updateAlert} toDos={toDos} addToDo={addToDo} deleteToDo={deleteToDo} completeToDo={completeToDo} /></Route>
                   <Route path="/"><Home updateAlert={updateAlert} toDos={toDos} addToDo={addToDo} deleteToDo={deleteToDo} completeToDo={completeToDo} /></Route>
                 </Switch>
               </BrowserRouter>
+            ) : (
+              <Button
+                href={`https://${config.cognito_hosted_domain}/login?response_type=token&client_id=${config.aws_user_pools_web_client_id}&redirect_uri=${config.redirect_url}`}
+                color="primary"
+                className="mt-5 float-center"
+              >
+                Log In
+              </Button>
+            )
+            }
           </Col>
         </Row>
     </div >
