097f447a8502c83a8b88849799f64b70f72217f6
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-04-28 17:33:55 +1000

    this is silly

--- www/src/HireRecord.js
+++ www/src/HireRecord.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { Button, ButtonGroup, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 
 function HireRecord({ updateToDo, toDos }) {
@@ -23,49 +24,62 @@ function HireRecord({ updateToDo, toDos }) {
                                                     Edit
                                                 </Button>
                         </div>
+                        
                         <div class="panel-container show">
                             <div class="panel-content">
 
-                                <div class="row" id="displayDiv">
-                                    <div class="col-md-3 fw-700 "> Date hired </div>
-                                    <div class="col-md-9">{new Date(hireRecord.creation_date).toLocaleDateString()}</div>
-                                </div>
-
-                                <div class="form-group editDiv" id="editDiv" >
-                                        <label class="form-label" for="simpleinput">  Date hired  </label>
-                                        <Input type="text" class="form-control" name="name" id="newToDoName" placeholder="name" />
+                                <form>
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Student Name </label>
+                                        <Input type="text" class="form-control" name="name" id="newToDoName"
+                                        value={hireRecord.name} placeholder="name" />
                                     </div>
-                                
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Name </div>
-                                    <div class="col-md-9">{hireRecord.name}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Code </div>
-                                    <div class="col-md-9">{hireRecord.code}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Instrument </div>
-                                    <div class="col-md-9">{hireRecord.instrument}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Brand </div>
-                                    <div class="col-md-9">{hireRecord.brand}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Rate </div>
-                                    <div class="col-md-9">{hireRecord.rate}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Owner </div>
-                                    <div class="col-md-9">{hireRecord.owner}</div>
-                                </div>
-                                <div class="row" >
-                                    <div class="col-md-3 fw-700"> Due Date </div>
-                                    <div class="col-md-9">{new Date(hireRecord.due).toLocaleDateString()}</div>
-                                </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Code</label>
+                                        <Input type="text" name="code" id="newToDoCode" placeholder="code"
+                                        value={hireRecord.code}/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Instrument</label>
+                                        <Input type="text" name="code" id="newToDoInstrument" placeholder="instrument"
+                                        value={hireRecord.instrument}/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Brand</label>
+                                        <Input type="text" name="brand" id="newToDoBrand" placeholder="brand" 
+                                        value={hireRecord.brand}/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Rate</label>
+                                        <Input type="text" name="rate" id="newToDoRate" placeholder="rate"
+                                        value={hireRecord.rate}/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Owner</label>
+                                        <Input type="text" name="owner" id="newToDoOwner" placeholder="owner" 
+                                        value={hireRecord.owner}/>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="simpleinput">Due Date</label>
+                                        <Input class="form-control" id="newToDoDueDate" type="date" name="date" placeholder="dd/mm/yyyy" value={hireRecord.due}/>
+                                    </div>
+
+                                    <Button onClick={updateToDo} class="btn btn-lg btn-primary waves-effect waves-themed mr-2 " className="ml-1">Update</Button>
+                                    <Link to="/"><Button class="btn btn-lg btn-default waves-effect waves-themed " className="ml-1">Cancel</Button></Link>
+
+
+
+
+                                </form>
                             </div>
                         </div>
+                  
                     </div>
                 </div>
             </div>
