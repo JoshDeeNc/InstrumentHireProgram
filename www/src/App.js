b8dd6b39bb06926a09760d3c8b8a1cb8868511de
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-04-29 19:51:13 +1000

    Added stuff

--- www/src/App.js
+++ www/src/App.js
@@ -149,14 +149,14 @@ function App() {
       || !brand || brand === '' || !rate || rate === '' || !owner || owner === '' || !dueDate || dueDate === '') return;
       */
     const updateToDo = {
-      "name": 'juanChanged4',
+      "name": 'juanChanged5',
       "completed": true,
       "code": '123456',
       "instrument": 'Euphonium',
       "brand": 'Jupiter',
-      "rate": '$560'
-      /*"owner": 'OMA',
-      "due": '30/04/2021'*/
+      "rate": '$560',
+      "owner": 'OMA',
+      "due": '30/04/2021'
     };
 
     const result = await axios({
