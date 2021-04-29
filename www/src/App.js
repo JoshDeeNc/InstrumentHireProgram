5b2b0451c3e945aa35590488af9b6f12692523bb
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-04-29 21:42:44 +1000

    last change

--- www/src/App.js
+++ www/src/App.js
@@ -156,7 +156,7 @@ function App() {
       "brand": 'Jupiter',
       "rate": '$560',
       "owner": "Dayspring Church",
-      "due": '2021/05/12'
+      "due": '2021-05-12'
     };
 
     const result = await axios({
