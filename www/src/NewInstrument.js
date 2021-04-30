35a823ba4c016abc6b8d8e22c1855815e5dabe1f
JoshDeeNc <joshua.dodanduwa@students.mq.edu.au>
2021-04-30 15:14:10 +1000

    UI Changes

--- www/src/NewInstrument.js
+++ www/src/NewInstrument.js
@@ -4,13 +4,13 @@ import { Button, ButtonGroup, Form, FormGroup, Input, Label, Row, Col } from 're
 function NewInstrument({ addInstrument }) {
     return (
         <div>
-            
+
             <div class="subheader">
-                    <h1 class="subheader-title">
-                       Add Instrument
+                <h1 class="subheader-title">
+                    Add Instrument
 
                     </h1>
-                </div>
+            </div>
             <div class="row">
                 <div class="col-md-10">
                     <div id="panel-1" class="panel">
@@ -27,57 +27,74 @@ function NewInstrument({ addInstrument }) {
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Code</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="code" id="newInstCode" placeholder="code" />
+                                            <Input type="text" class="form-control" name="code" id="newInstCode" placeholder="code" />
                                         </div>
                                     </div>
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Instrument/Add-on Type </label>
                                         <div class="col-sm-10">
-                                        <Input type="text" class="form-control" name="name" id="newInstType" placeholder="type" />
+                                            <Input type="text" class="form-control" name="name" id="newInstType" placeholder="type" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Object Name </label>
                                         <div class="col-sm-10">
-                                        <Input type="text" class="form-control" name="name" id="newInstName" placeholder="instrument/add-on" />
+                                            <Input type="text" class="form-control" name="name" id="newInstName" placeholder="instrument/add-on" />
                                         </div>
                                     </div>
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Brand</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="brand" id="newInstBrand" placeholder="brand" />
+                                            <Input type="text" class="form-control" name="brand" id="newInstBrand" placeholder="brand" />
                                         </div>
                                     </div>
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Rate</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="rate" id="newInstRate" placeholder="rate" />
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text">$</span>
+                                                </div>
+                                                <Input type="text" class="form-control" name="rate" id="newInstRate" placeholder="rate" />
+                                            </div>
                                         </div>
                                     </div>
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Purchase Cost</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="rate" id="newInstPurchVal" placeholder="cost" />
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text">$</span>
+                                                </div>
+                                                <Input type="text" class="form-control" name="rate" id="newInstPurchVal" placeholder="cost" />
+                                            </div>
                                         </div>
                                     </div>
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Depreciation Rate</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="rate" id="newInstDepr" placeholder="depreciation" />
+                                            <div class="input-group">
+                                                <Input type="text" class="form-control" name="rate" id="newInstDepr" placeholder="depreciation" />
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">%</span>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
+
+
 
                                     <div class=" form-group row">
                                         <label class="col-sm-2 col-form-label" for="simpleinput">Owner</label>
                                         <div class="col-sm-10">
-                                        <Input type="text" name="owner" id="newInstOwner" placeholder="owner" />
-                                        </div> 
+                                            <Input type="text" name="owner" id="newInstOwner" placeholder="owner" />
+                                        </div>
                                     </div>
 
                                     <Button onClick={addInstrument} class="btn btn-lg btn-primary waves-effect waves-themed mr-2 " className="ml-1">Add</Button>
@@ -106,11 +123,11 @@ export default NewInstrument;
                                                     <label class="form-label" for="example-select">Instrument</label>
                                                     <select class="form-control" id="example-select">
                                                         <option>Electric Guitar</option>
-														<option>Bass Guitar</option>
-														<option>Accoustic Guitar</option>
+                                                        <option>Bass Guitar</option>
+                                                        <option>Accoustic Guitar</option>
                                                         <option>Flute</option>
                                                         <option>Violin</option>
                                                         <option>Trumpet</option>
-                                                      
+
                                                     </select>
                                                 </div>*/
