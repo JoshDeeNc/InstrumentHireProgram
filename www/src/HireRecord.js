a8e0df5a529cb65bcbc7dbed1048f8dca9c185be
JoshDeeNc <joshua.dodanduwa@students.mq.edu.au>
2021-05-01 07:42:43 +1000

    UI Updates

--- www/src/HireRecord.js
+++ www/src/HireRecord.js
@@ -74,6 +74,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input type="text" class="form-control nox" name="name" id="editToDoName"
                                                 value={studName} onChange={onChangeStud} placeholder="name" />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>    
                                     </div>
 
                                     <div class="form-group row">
@@ -82,6 +83,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input type="text" class="form-control nox" name="code" id="editToDoCode" placeholder="code"
                                                 value={code} onChange={onChangeCode} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
 
                                     <div class="form-group row">
@@ -90,6 +92,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input class="form-control nox" type="text" name="code" id="editToDoInstrument" placeholder="instrument"
                                                 value={instrument} onChange={onChangeInstr} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
 
                                     <div class="form-group row ">
@@ -98,6 +101,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input class="form-control nox" type="text" name="brand" id="editToDoBrand" placeholder="brand"
                                                 value={brand} onChange={onChangeBrand} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
 
                                     <div class="form-group row">
@@ -106,6 +110,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input class="form-control nox" type="text" name="rate" id="editToDoRate" placeholder="rate"
                                                 value={rate} onChange={onChangeRate} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
 
                                     <div class="form-group row">
@@ -114,6 +119,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input class="form-control nox" type="text" name="owner" id="editToDoOwner" placeholder="owner"
                                                 value={owner} onChange={onChangeOwner} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
 
                                     <div class="form-group row">
@@ -122,6 +128,7 @@ function HireRecord({ deleteToDo, updateToDo, toDos }) {
                                             <input class="form-control nox"  id="editToDoDueDate" type="date" name="date"
                                                 placeholder="dd/mm/yyyy" value={dueDate} onChange={onChangeDue} />
                                         </div>
+                                        <div class="col-sm-12 hr"></div>  
                                     </div>
                                     <div class="btn-div">
                                         <Button data-item-id={hireRecord.id} onClick={(e) => updateToDo(hireRecord.id)} class="btn btn-lg btn-primary waves-effect waves-themed mr-2">Update</Button>
