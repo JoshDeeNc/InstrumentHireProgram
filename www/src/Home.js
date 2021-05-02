9655eb2b6438f9709649da03a8e15e7a65be3b34
JoshDeeNc <joshua.dodanduwa@students.mq.edu.au>
2021-05-02 17:47:47 +1000

    Update Home.js

--- www/src/Home.js
+++ www/src/Home.js
@@ -51,10 +51,10 @@ function Home({ toDos, deleteToDo, completeToDo }) {
                   <div id="panel-1" class="panel mb-0">
                     <div class="panel-hdr">
                       <div class="row  ">
-                        <div class="col-md-4"> <h2> Hired Instrument List</h2></div>
-                        <div class="col-md-4"> <input type="text" value={q} onChange={(e) => setQ(e.target.value)} class="form-control" placeholder="search..." />
+                        <div class="col-md-3"> <h2> Hired Instrument List</h2></div>
+                        <div class="col-md-2"> <input type="text" value={q} onChange={(e) => setQ(e.target.value)} class="form-control mt-2" placeholder="search..." />
                         </div>
-                        <div class="col-md-4">    <Link to="/newhire"><Button color="primary" className="ml-1">New Hire</Button></Link>
+                        <div class="col-md-7">    <Link to="/newhire"><Button color="primary" className="ml-1">New Hire</Button></Link>
                           <Link to="/studentlist"><Button color="primary" className="ml-1">Students</Button></Link>
                           <Link to="/instrumentlist"><Button color="primary" className="ml-1">Instruments</Button></Link>
                         </div>
