8a6639f4bf52c95d1b2dc56f57d8f77097c1ad9e
JoshDeeNc <joshua.dodanduwa@students.mq.edu.au>
2021-05-04 16:19:05 +1000

    removed testfile for now

--- www/src/custom.js
+++ www/src/custom.js
@@ -18,3 +18,98 @@
       });
     }, false);
   })();
+
+
+
+  
+  var lineChart = function()
+  {
+      var config = {
+          type: 'line',
+          data:
+          {
+              labels: ["January", "February", "March", "April", "May", "June", "July"],
+              datasets: [
+              {
+                  label: "Success",
+                  borderColor: color.success._500,
+                  pointBackgroundColor: color.success._700,
+                  pointBorderColor: 'rgba(0, 0, 0, 0)',
+                  pointBorderWidth: 1,
+                  borderWidth: 1,
+                  pointRadius: 3,
+                  pointHoverRadius: 4,
+                  data: [
+                      23,
+                      75,
+                      60, -48, -9,
+                      26,
+                      45
+                  ],
+                  fill: false
+              }]
+          },
+          options:
+          {
+              responsive: true,
+              title:
+              {
+                  display: false,
+                  text: 'Line Chart'
+              },
+              tooltips:
+              {
+                  mode: 'index',
+                  intersect: false,
+              },
+              hover:
+              {
+                  mode: 'nearest',
+                  intersect: true
+              },
+              scales:
+              {
+                  xAxes: [
+                  {
+                      display: true,
+                      scaleLabel:
+                      {
+                          display: false,
+                          labelString: '6 months forecast'
+                      },
+                      gridLines:
+                      {
+                          display: true,
+                          color: "#f2f2f2"
+                      },
+                      ticks:
+                      {
+                          beginAtZero: true,
+                          fontSize: 11
+                      }
+                  }],
+                  yAxes: [
+                  {
+                      display: true,
+                      scaleLabel:
+                      {
+                          display: false,
+                          labelString: 'Profit margin (approx)'
+                      },
+                      gridLines:
+                      {
+                          display: true,
+                          color: "#f2f2f2"
+                      },
+                      ticks:
+                      {
+                          beginAtZero: true,
+                          fontSize: 11
+                      }
+                  }]
+              }
+          }
+      };
+      new Chart($("#lineChart > canvas").get(0).getContext("2d"), config);
+  }
+ 
