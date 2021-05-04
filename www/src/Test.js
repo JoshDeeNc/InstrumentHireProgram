1bd878cdf223339bfd37072aed2bfc3cd0b86e91
JoshDeeNc <joshua.dodanduwa@students.mq.edu.au>
2021-05-04 16:20:35 +1000

    readded test

---
+++ www/src/Test.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Button, ButtonGroup,   Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormFeedback, FormText } from 'reactstrap';
+
+function Test() {
+    return (
+        <div> 
+  <h1>Test</h1>
+  <div id="lineChart">
+       <canvas class="chart-div bdr"></canvas>
+    </div>
+    </div>
+  );
+}
+
+
+
+
+export default Test;
