853396c215e528cc39a25acd721a4abe2482f771
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-05-04 22:16:57 +1000

    temporary

--- www/src/Home.js
+++ www/src/Home.js
@@ -55,8 +55,8 @@ function Home({ toDos, deleteToDo, completeToDo }) {
     var dates = document.getElementById("datepicker-1").value;
     var startEnd = dates.split(" - ")
     console.log(startEnd)
-    setRecords(toDos.filter(item => new Date(item.creation_date) >= new Date(startEnd[0]) && new Date(item.creation_date) <= new Date(startEnd[1])))
-    console.log(hireRecords)
+    return hireRecords.filter(item => new Date(item.creation_date) >= new Date(startEnd[0]) && new Date(item.creation_date) <= new Date(startEnd[1]))
+    //console.log(hireRecords)
   }
 
   return (
