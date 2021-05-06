d8b42ed9d4fe44591ae4a695b255e47d1f9be026
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-05-06 16:37:06 +1000

    trying something

--- www/src/NewHire.js
+++ www/src/NewHire.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Link, Route, Switch, useHistory } from 'react-router-dom
 import { Button, ButtonGroup, Form, FormGroup, Input, Label, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 function NewHire({ toDos, studentList, instInventory, addToDo }) {
 
-    const inventory = instInventory.map(item => item.available === true)
+    const inventory = instInventory
 
     const history = useHistory();
 
