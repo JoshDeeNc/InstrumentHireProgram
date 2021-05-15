8252032787c870437e23b367138e5547f0e14fb6
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-05-15 21:49:17 +1000

    retrying addons

--- www/src/NewHire.js
+++ www/src/NewHire.js
@@ -237,7 +237,7 @@ function NewHire({ toDos, studentList, instInventory, instOptionsList, addToDo }
                                                                     <div class=" hr-2"></div>
                                                                 </div>
                                                             </div>
-                                                            {addOns.map((item, index) => (
+                                                            {addOns.length > 0 ? addOns.map((item, index) => (
                                                             <div class=" form-group row">
                                                                 <label class="col-8 col-form-label" for="simpleinput">{addOns[item]}</label>
                                                                 <div class="col-2">
@@ -248,7 +248,7 @@ function NewHire({ toDos, studentList, instInventory, instOptionsList, addToDo }
                                                                 <div class="col-sm-12 ">
                                                                     <div class=" hr"></div>
                                                                 </div>
-                                                            </div>))}
+                                                            </div>)) : (<div></div>)}
                                                        
                                                        
                                                         </div>
