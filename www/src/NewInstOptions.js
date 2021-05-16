1a980881235a3b923670c39b2fc2c3271916b854
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-05-16 20:38:10 +1000

    small UI changes

--- www/src/NewInstOptions.js
+++ www/src/NewInstOptions.js
@@ -28,15 +28,18 @@ function NewInstOptions({ addInstOptions }) {
 
     return (
         <div>
-            <h1>New Instrument Type</h1>
 
+<div class="subheader"><h1 class="subheader-title">New Instrument Type</h1></div>
             <div class="row">
                 <div class="col-lg-7 col-md-10">
                     <div id="panel-1" class="panel">
                         <div class="panel-hdr-dsp">
+                        <span onClick={() => history.goBack()}><i class="fal fn-icon fa-arrow-circle-left hand mr-2"></i></span>
                             <h2>New Instrument Type</h2>
                             <div></div>
                         </div>
+
+
                         <div class="panel-container show">
                             <div class="panel-content">
 
@@ -61,9 +64,7 @@ function NewInstOptions({ addInstOptions }) {
                                         </div>
 
 
-                                        <div class="col-sm-12 mb-2 mt-2 ">
-                                            <div class=" hr"></div>
-                                        </div>
+                                        
                                     </div>
 
                                     <div class=" form-group row">
@@ -71,18 +72,15 @@ function NewInstOptions({ addInstOptions }) {
                                         <div class="col-sm-9">
                                             <Input type="text" class="form-control no-edit" name="code" id="newAddOnsList" />
                                         </div>
-                                        <div class="col-sm-12 ">
-                                            <div class=" hr"></div>
-                                        </div>
+                                       
 
-                                        <div class="col-sm-12 mb-2 mt-2 ">
-                                            <div class=" hr"></div>
-                                        </div>
+                                       
                                     </div>
 
 
-                                    <div class="btn-divS">
+                                    <div class="btn-divS mt-3">
                                         <Button onClick={add} color="primary" className="mr-2" >Add</Button>
+                                        <Link to="/instoptionslist"><Button color="secondary"  >Cancel</Button></Link>
 
                                     </div>
                                 </form>
