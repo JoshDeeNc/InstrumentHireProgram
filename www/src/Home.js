1da3ec2a3b84bc8ae65e928349f878f438a0a02d
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-06-22 13:01:43 +1000

    trying something

--- www/src/Home.js
+++ www/src/Home.js
@@ -8,6 +8,8 @@ import history from './history';
 
 function Home({ toDos, deleteToDo }) {
 
+  toDos = toDos.map(item => Object.assign({}, item, { creation_date: new Date(item.creation_date).toLocaleDateString()}))
+
   const curHires = toDos.filter(item => item.returned === "")
   const overDues = toDos.filter(item => item.returned === "" && new Date(item.due) < new Date())
   const returnedHires = toDos.filter(item => item.returned != "")
@@ -191,7 +193,7 @@ function Home({ toDos, deleteToDo }) {
                             <tbody>
                               {search(curHires).map((item, index) => (
                                 <tr role="row" key={item.id}>
-                                  <td>{new Date(item.creation_date).toLocaleDateString()}</td>
+                                  <td>{item.creation_date}</td>
                                   <td> {item.name}</td>
                                   <td>{item.code}</td>
                                   <td>{item.instrument}</td>
@@ -287,7 +289,7 @@ function Home({ toDos, deleteToDo }) {
                                   <td>{item.instrument}</td>
                                   <td>{item.brand}</td>
                                   <td>${item.rate}</td>
-                                  <td>{new Date(item.creation_date).toLocaleDateString()}</td>
+                                  <td>{item.creation_date}</td>
                                   <td>{item.owner}</td>
                                   <td class="al-ctr"><Link to={`/hirerecord/${item.id}`}><i class="fal fa-2x fa-arrow-circle-right float-right"></i></Link></td>
                                 </tr>
@@ -370,7 +372,7 @@ function Home({ toDos, deleteToDo }) {
                               {search(returnedHires).map((item, index) => (
                                 <tr role="row" key={item.id}>
                                   <td>{new Date(item.returned).toLocaleDateString()}</td>
-                                  <td>{new Date(item.creation_date).toLocaleDateString()}</td>
+                                  <td>{item.creation_date}</td>
                                   <td>{item.name}</td>
                                   <td>{item.code}</td>
                                   <td>{item.instrument}</td>
